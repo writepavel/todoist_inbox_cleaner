ee70f9fab90d67ca98a2b8a8b4909fbf0c570e76
Pavel Ponomarev <writepavel@gmail.com>
2020-10-23 22:07:02 +0200

    miro lib has catched inside clojurescript

---
+++ src/inboxcleaner/core.cljs
@@ -0,0 +1,11 @@
+(ns inboxcleaner.core
+)
+
+(defonce icon
+  "<circle cx= \"12\" cy= \"12\" r= \"9\" fill= \"none\" fill-rule= \"evenodd\" stroke= \"currentColor\" stroke-width= \"2\" ></circle>")
+
+(defn add-button
+  []
+(js/console.log js/miro))
+
+;; (shadow/repl :app)
