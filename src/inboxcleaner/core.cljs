62572ac680747aad931109c16e174fb0cff1f798
Pavel Ponomarev <writepavel@gmail.com>
2020-10-29 07:59:56 +0100

    list of widgets and create-sample-card implemented

--- src/inboxcleaner/core.cljs
+++ src/inboxcleaner/core.cljs
@@ -1,6 +1,5 @@
 ;; port of miro sample plugin to clojure script https://github.com/miroapp/app-examples/blob/master/buttons/signedin.html
 
-
 (ns inboxcleaner.core
   (:require
    [kitchen-async.promise :as p]
@@ -12,6 +11,71 @@
 
 (defonce icon
          "<circle cx= \"12\" cy= \"12\" r= \"9\" fill= \"none\" fill-rule= \"evenodd\" stroke= \"currentColor\" stroke-width= \"2\" ></circle>")
+
+(def appstate (atom {:frameInbox nil
+                     :frameIdeas nil 
+                     :frameTasks nil 
+                     :frameReference nil 
+                     :frameInProjectPlan nil 
+                     :frameToDecompose nil 
+                     :frameDone nil}))
+
+(defn ->cljs
+  [js-object]
+(js->clj js-object :keywordize-keys true))
+
+;; TODO. Consider creating macro js-await 
+;; automatically converting result of promise to cljs.
+;; But I don't want to create a go block each time..
+;; Probably will use transducer to use results without need of go block  usage.
+
+(defn clear-board
+  []
+  (go (let [all-widgets (->cljs (<p! (.. js/miro -board -widgets get)))
+            widgetIds   (mapv #(get % :id) all-widgets)]
+        (println widgetIds))))
+
+(comment
+  
+  (take! (go 
+           (let [all-widgets (->cljs (<p! (.. js/miro -board -widgets get)))
+                 ws (mapv #(get % :id) all-widgets)]
+              ws))
+         #(println %)
+        ;;  #(js/console.log (js-obj %))
+         ))
+
+
+(defn create-frames
+  []
+)
+
+(defn reload-todoist-inbox
+  []
+(go 
+  ;; (clear-board)
+  (create-frames)))
+
+;; TODO create macros for creating widgets. 
+;; It hides js api and converts to js data structures.
+;; Think about accuracy with go blocks and using transducers as well.
+
+(defn create-sample-card
+  [title project]
+(go 
+  (<p! (.create (.. js/miro -board -widgets) 
+            #js {:type  "card"
+                 :title "Hello"
+                 :scale 0.7
+                 :card  
+                 #js {:customFields 
+                      [ #js {:value     "project"
+                             :mainColor "#ABEBC6"
+                             :fontColor "#5B2C6F"
+                             }]}}
+                )
+       
+       )))
 
 (defn sayHi
   []
